e56cb7f7690a70a45f22c85aa8f66ea7747d42b6
Falkai Péter <h756828@stud.u-szeged.hu>
2019-12-22 12:40:20 +0100

    creating essential components and project structure, adding ngmaterial and firebase via npm

--- MyTourApp/src/app/app.module.ts
+++ MyTourApp/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavMainComponent } from './components/nav/nav-main/nav-main.component';
+import { HomeComponent } from './pages/home/home.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { DiscoverComponent } from './pages/discover/discover.component';
+import { PlanningComponent } from './pages/planning/planning.component';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMainComponent,
+    HomeComponent,
+    WelcomeComponent,
+    ProfileComponent,
+    DiscoverComponent,
+    PlanningComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
